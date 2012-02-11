765a248e617b334b5d317a651a12aaa7efcdb065
Lucas Paulger <lucas.paulger@gmail.com>
2012-02-10 22:49:28 -0500

    added correct fetch url for api on tasks

--- app/models/taskmodel.js
+++ app/models/taskmodel.js
@@ -1,8 +1,8 @@
 window.Task = Backbone.Model.extend({
-    url: "http://devdashtest.local/api/index.php/api/task"
+    url: "http://devdash.local/api/index.php/api/task"
 });
     
 window.TaskCollection = Backbone.Collection.extend({
     model: Task,
-    url: "http://devdashtest.local/api/index.php/api/tasks"
+    url: "http://devdash.local/api/index.php/api/tasks"
 });
